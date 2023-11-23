ccc6412e712edcdc62fccd5c5e8fa807f077d46c
Ron Unrau <rcunrau@ualberta.ca>
2023-11-23 14:38:09 -0700

    initial checkin

---
+++ src/component.rs
@@ -0,0 +1,96 @@
+use crate::message::{self, Command, Message};
+
+pub struct Eps {
+    uart: u8,
+    state: u8,
+}
+
+impl Eps {
+    fn configure() -> Eps {
+        Eps { uart: 7, state: 1 }
+    }
+
+    fn dispatch(&mut self, cmd: &Command) -> Result<Message, &'static str> {
+        println!("Eps: opcode {0} uart {1}", cmd.opcode, self.uart);
+        if cmd.oplen > 0 {
+            let arg = std::str::from_utf8(&cmd.opdata).unwrap();
+            println!("Eps: op data: {}", arg);
+        }
+        Ok(cmd.status_msg(self.state))
+    }
+}
+
+pub struct Adcs {
+    port: i32,
+}
+
+impl Adcs {
+    fn configure(args: i32) -> Adcs {
+        Adcs { port: args }
+    }
+
+    fn dispatch(&mut self, cmd: &Command) -> Result<Message, &'static str> {
+        self.port = -self.port;
+        println!("Adcs: opcode {0}, port {1}", cmd.opcode, self.port);
+
+        Ok(cmd.status_msg(42))
+    }
+}
+
+pub struct Dfgm {
+    port: u16,
+    state: u8,
+}
+
+impl Dfgm {
+    fn configure() -> Dfgm {
+        Dfgm { port: 1, state: 0 }
+    }
+
+    fn dispatch(&mut self, cmd: &Command) -> Result<Message, &'static str> {
+        if cmd.oplen > 0 {
+            let arg = std::str::from_utf8(&cmd.opdata).unwrap();
+            println!("dfgm op data: {0}", arg);
+            match arg.parse::<u16>() {
+                Ok(i) => self.port = i,
+                Err(s) => println!("Parse failed: {}", s),
+            }
+        }
+        println!("Dfgm: opcode {}, port: {}", cmd.opcode, self.port);
+        self.state += 1;
+        Ok(cmd.status_msg(self.state))
+    }
+}
+
+
+pub enum Component {
+    Root,
+    Eps(Eps),
+    Adcs(Adcs),
+    Dfgm(Dfgm),
+}
+
+pub fn init() -> Vec<Component> {
+    let mut components: Vec<Component> = Vec::new();
+
+    for (index, p) in message::PAYLOADS.iter().enumerate() {
+        assert_eq!(index, p.id, "payload index/id mismatch");
+        match p.name {
+            message::PAYLOAD_EPS => components.push(Component::Eps(Eps::configure())),
+            message::PAYLOAD_ADCS => components.push(Component::Adcs(Adcs::configure(7))),
+            message::PAYLOAD_DFGM => components.push(Component::Dfgm(Dfgm::configure())),
+            _ => components.push(Component::Root),
+        }
+    }
+ 
+   components
+}
+    
+pub fn dispatch_cmd(target: &mut Component, cmd: &Command) -> Result<Message, &'static str> {
+    match target {
+        Component::Eps(eps) => eps.dispatch(cmd),
+        Component::Adcs(adcs) => adcs.dispatch(cmd),
+        Component::Dfgm(dfgm) => dfgm.dispatch(cmd),
+        _ => Err("Unrecognized component")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+use clap::Parser;
+use crate::message::Message;
+
+mod message;
+mod component;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional IP address to use
+    host: Option<String>,
+
+    /// Port to listen on
+    #[arg(short, long)]
+    port: i32,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let host = match &cli.host {
+        Some(hostname) => hostname,
+        None => "localhost",
+    };
+
+    let port = cli.port;
+    let ipaddr = host.to_owned() + ":" + &port.to_string();
+    let listener = TcpListener::bind(ipaddr).unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    println!("Server listening on {host}:{port}");
+
+    let mut components = component::init();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                handle_client(stream, &mut components);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
+}
+
+fn handle_client(mut stream: TcpStream, components: &mut [component::Component]) {
+    let mut data = [0; message::MSG_LEN];
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            match size {
+                0 => { println!("EOF");
+                       false
+                }
+                _ => {
+                    println!("read {size} bytes");
+                    let response = handle_message(&data, components);
+                    match stream.write(&response) {
+                        Ok(wlen) => if size < message::MSG_LEN {
+                            println!("short write: {wlen}");
+                            false
+                        }
+                        else {
+                            true
+                        },
+                        Err(e) => {
+                            println!("write error: {e}");
+                            false
+                        }
+                    }
+                }
+            }
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection");
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn handle_message(msg: &Message, components: &mut [component::Component]) -> Message {
+    let cmd = message::Command::deserialize(msg);
+
+    println!("handle_message: payload {0}, opcode {1}", cmd.payload, cmd.opcode);
+
+    let target = &mut components[cmd.payload.id];
+    match component::dispatch_cmd(target, &cmd) {
+        Ok(msg) => msg,
+        Err(_) => cmd.status_msg(255)
+    }
+}
